96ae2f6b28fb4ba1bac53a24edd8e0f6b505a95a
Ayan Abbas <ayanabbas727@gmail.com>
2025-01-12 22:23:00 +0530

    implementing indexes

--- myvcs.py
+++ myvcs.py
@@ -1,4 +1,5 @@
 import os,hashlib
+import collections,struct
 print("Hello")
 def init(repo):
     os.mkdir(repo)
@@ -14,7 +15,7 @@ def init(repo):
 def hash_object(data,obj_type,write = True):
     header = '{} {}'.format(obj_type,len(data)).encode()
     full_data = header + b'\x00' + data
-    sha1 = hashlib.sha1(full_data).hexdigest()
+    sha1 = hashlib.sha256(full_data).hexdigest()
     if write:
         path = os.path.join('.git','objects',sha1[:2],sha1[2:])
         if not os.path.exists(path):
@@ -22,7 +23,55 @@ def hash_object(data,obj_type,write = True):
             # write_file(path,zlib.compress(full_data))
     return sha1
 
-# init("Abbas")
-file_content = "My name is Abbas Bhai"
-temp = hash_object(file_content.encode(), 'blob', write=False)
-print(temp)
+# # init("Abbas")
+# file_content = "My name is Abbas Bhai"
+# temp = hash_object(file_content.encode(), 'blob', write=False)
+# print(temp)
+
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode', 'uid',
+    'gid', 'size', 'sha1', 'flags', 'path',
+])
+
+def read_file(path):
+    """Read contents of file at given path as bytes."""
+    with open(path, 'rb') as f:
+        return f.read()
+
+
+def write_file(path, data):
+    """Write data bytes to file at given path."""
+    with open(path, 'wb') as f:
+        f.write(data)
+
+def read_index():
+    try:
+        data = read_file(os.path.join('.git','index'))
+    except FileNotFoundError:
+        return []
+    digest = hashlib.sha1(data[:-20]).digest()
+    assert digest == data[-20:], 'invalid index checksum'
+    signature,version,num_entries = struct.unpack('!4sLL',data[:12])
+    assert signature == b'DIRC',\
+            'invalid index signature {}'.format(signature)
+
+    assert version == 2, 'unknown index version {}'.format(version)
+    entry_data = data[12:-20]
+    entries = []
+    i = 0
+    while i + 62 < len(entry_data):
+        fields_end = i + 62
+        fields = struct.unpack('!LLLLLLLLLL20sH',
+                                entry_data[i:fields_end])
+        path_end = entry_data.index(b'\x00', fields_end)
+        path = entry_data[fields_end:path_end]
+        entry = IndexEntry(*(fields + (path.decode(),)))
+        entries.append(entry)
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+
+    assert len(entries) == num_entries
+    return entries
+
+
+print(read_index())
